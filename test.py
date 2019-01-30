36500de50c30ac6aea74415a63e085d4d07b3d6a
Deji Kadri <dejikadri@gmail.com>
2019-01-30 23:32:48 +0100

    changed assertEquals to assertEqual

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from src.calc2 import Calc as clc
 
 class CalcTst(unittest.TestCase):
     def test_add(self):
-        self.assertEquals(clc.add(2, 6), 18)
+        self.assertEqual(clc.add(2, 6), 18)
